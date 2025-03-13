3ea35eb6e127d81330eaed8f99f911ab0cad58b4
Priyanshu-Gupta777 <priyanshugp777@gmail.com>
2025-03-13 20:46:58 +0530

    only testing

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -o errexit
 
-echo "Installing system dependencies (ffmpeg, portaudio)..."
-apt update && apt install -y ffmpeg portaudio19-dev
+# Install system dependencies
+apt-get update && apt-get install -y portaudio19-dev
 
-echo "Installing Python dependencies..."
+# Install Python dependencies
 pip install --upgrade pip
-pip install --no-cache-dir python-dotenv
-pip install --no-cache-dir -r requirements.txt
-
-echo "Running the chatbot application..."
-python app.py
+pip install -r requirements.txt
